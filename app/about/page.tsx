02328837ca9d372e7e09bb86c030880dbad9ee3d
John Doan <csdoanj94@gmail.com>
2026-01-25 15:30:19 -0500

    Created Routes + Images

---
+++ app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function LoginPage() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>About</h1>
+      <p>This page will display your about information.</p>
+    </main>
+  );
+}
---
+++ app/contact/page.tsx
@@ -0,0 +1,8 @@
+export default function LoginPage() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>Contact</h1>
+      <p>This page will display your contact information.</p>
+    </main>
+  );
+}
